b388143e67b0a53d1f10afdbe64493c212c78020
Ferguson Osas <ebenezerosas92@gmail.com>
2022-03-13 17:54:02 -0400

    Initialise repo

---
+++ src/BreathFirstSearch.java
@@ -0,0 +1,22 @@
+public class BreathFirstSearch {
+
+    static class Vertex{
+
+//        Vertex (){
+//
+//        }
+
+//      String name;
+        String color;
+        int distance = 43;
+        Vertex parent;
+        Vertex [] adjVertex = {};
+    }
+
+    public static void main(String[] args) {
+
+        Vertex a = new Vertex();
+
+        System.out.println(a.distance);
+    }
+}
